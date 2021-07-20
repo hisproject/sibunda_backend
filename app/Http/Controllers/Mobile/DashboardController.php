56a6ef645c9aeb1ccd8f8375a4852b20563f334a
Amir Mu'tashim Billah <34206012+amirkode@users.noreply.github.com>
2021-07-20 23:53:50 +0700

    amir : change home data

--- app/Http/Controllers/Mobile/DashboardController.php
+++ app/Http/Controllers/Mobile/DashboardController.php
@@ -19,29 +19,29 @@ class DashboardController extends Controller
         ];
         $data['kesehatan_keluarga'] = [
             [
-                'img_url' => '/res/img/home/kesehatan_keluarga_1.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/kesehatan_keluarga_1.png',
                 'desc' => 'Selamat Berat Badan Bunda Normal'
             ],
             [
-                'img_url' => '/res/img/home/kesehatan_keluarga_2.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/kesehatan_keluarga_2.png',
                 'desc' => 'Gerakan Anak Bunda Kurang Nih Bun!'
             ],
         ];
         $data['tips_dan_info'] = [
             [
-                'img_url' => '/res/img/home/tips_1.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/tips_1.png',
                 'desc' => 'Nih Bun 5 Makanan Rekomendasi untuk Bunda Hamil Trimester 2'
             ],
             [
-                'img_url' => '/res/img/home/tips_2.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/tips_2.png',
                 'desc' => 'Perkembangan Janin Usia 9 Minggu Kehamilan, Yuk Bun Ketahui Selengkapnya!'
             ],
             [
-                'img_url' => '/res/img/home/tips_3.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/tips_3.png',
                 'desc' => 'Yuk Bun Ketahui Pola Asuh Bayi Baru Lahir Sampai Usia 1,5 Tahun'
             ],
             [
-                'img_url' => '/res/img/home/tips_4.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/tips_4.png',
                 'desc' => 'Bagaimana Cara Memberikan ASI ke Bayi Baru Lahir 0-28 Hari (Neonatus)?'
             ],
         ];

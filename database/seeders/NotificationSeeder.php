a32969e0688e439a542cfea361fd7b844d2867dd
Amir Mu'tashim Billah <34206012+amirkode@users.noreply.github.com>
2021-07-21 01:36:12 +0700

    Amir :

--- database/seeders/NotificationSeeder.php
+++ database/seeders/NotificationSeeder.php
@@ -24,7 +24,7 @@ class NotificationSeeder extends Seeder
                 'is_message' => false,
                 'title' => 'Selamat Datang di SiBunda',
                 'desc' => 'Satu aplikasi untuk semua tahap kehamilan dan kesehatan bayi bunda, mulai dari usia 0 - 6 tahun',
-                'img_url' => '/res/img/home/notif_1.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/notif_1.png',
                 'url' => 'https://google.com',
                 'user_id' => 5
             ],
@@ -32,7 +32,7 @@ class NotificationSeeder extends Seeder
                 'is_message' => true,
                 'title' => 'Bunda, Pastikan Kehamilan Bunda Sehat Ya',
                 'desc' => 'Jangan lupa untuk periksa rutin ya, serta selalu isikan data perkembangan bunda dan calon buah hati di aplikasi ya.',
-                'img_url' => '/res/img/home/message_1.png',
+                'img_url' => 'https://sibunda.amirmb.com/res/img/home/message_1.png',
                 'url' => 'https://google.com',
                 'user_id' => 5
             ],
